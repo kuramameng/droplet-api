c1c27255e14062f146ae55198977fed02a305625
kuramameng <mengjs324@gmail.com>
2015-12-14 14:47:49 -0500

    Changes login methods for it to work for the frontend

--- routes/index.js
+++ routes/index.js
@@ -6,23 +6,27 @@ var authCtrl = require('../controllers/auth');
 router.get('/', authCtrl.root.get);
 
 /**AUTH ROUTES
- *	a login route using `passport.authenticate`
- *	a register route **not using passport**
+ *  a login route using `passport.authenticate`
+ *  a register route **not using passport**
  *
  */
 router.route('/login').
-	get(authCtrl.deny).
-	post(authCtrl.login.post);
+    get(authCtrl.deny).
+    post(authCtrl.login.post).
+    all(authCtrl.login.all);
 
 router.route('/logout').
-	all(authCtrl.logout.all);
+  all(authCtrl.logout.all);
 
 router.route('/changePassword').
-	get(authCtrl.deny).
-	patch(authCtrl.changePassword.patch);
+  get(authCtrl.deny).
+  patch(authCtrl.changePassword.patch);
 
 router.route('/signup').
-	get(authCtrl.deny).
-	post(authCtrl.signup.post);
+  get(authCtrl.deny).
+  post(authCtrl.signup.post);
+
+// router.route('/destroy').
+//   delete(authCtrl.destroy.delete);
 
 module.exports = router;
